8bcfe5246230d1b71281ef5e1da0259d7f2945cf
Angus Leung <prototype032@gmail.com>
2020-07-08 15:23:21 +1000

    plot phi as function of actual threshold value

--- phi_3/main_summary.m
+++ phi_3/main_summary.m
@@ -8,7 +8,14 @@ Summary figures
 
 %% Load
 
-load('results/split/unidir_no_inst_nSamples200000_nRuns10_medianSplit_binAverage/joined.mat');
+tpm_type = 'unidir_no_inst_nSamples200000_nRuns10_medianSplit_binAverage';
+tpm_type = 'unidirNoInstOrder1_nSamples200000_nRuns10_medianSplit_binAverage';
+%tpm_type = 'disconnectedNoInstOrder1_nSamples200000_nRuns10_medianSplit_binAverage';
+%tpm_type = 'bidirNoInstOrder1_nSamples200000_nRuns10_medianSplit_binAverage';
+tpm_type = 'NLbidirNoInstOrder1Thresh1_nSamples200000_nRuns10_percSplit_binAverage';
+%tpm_type = 'NLbidirNoInstOrder1Thresh1_nSamples200000_nRuns10_percSplit_binAverage_500perState';
+
+load(['results/split/' tpm_type '/joined.mat']);
 
 %% Plot phi as function of tau
 
@@ -22,6 +29,50 @@ hold on;
 % Plot average across runs
 plot(phis{1}.taus, mean(values, 1), 'LineWidth', 2);
 
-title('tau-bin using all offsets (median-split)');
+title(tpm_type);
 ylabel('\Phi');
 xlabel('\tau (ms)');
+
+ylim([0 0.15]); xlim([0 20]);
+
+%% Plot phi as function of percentile thresh
+
+figure;
+
+values = permute(phis{1}.phis, [3 5 1 2 4]);
+
+% Plot individual runs
+plot(phis{1}.threshs, values, ':');
+hold on;
+% Plot average across runs
+plot(phis{1}.threshs, mean(values, 1), 'LineWidth', 2);
+
+title(tpm_type);
+ylabel('\Phi');
+xlabel('thresh percentile');
+
+%ylim([0 0.15]); xlim([0 20]);
+
+%% Plot phi as function of actual threshold value
+
+params = load(['../tpms/' tpm_type '/params.mat']);
+
+% Assume consistent threshold values at each percentile, for both nodes
+threshs = mean(mean(params.thresh_values, 3), 2);
+
+values = permute(phis{1}.phis, [3 5 1 2 4]);
+
+figure;
+% Plot individual runs
+plot(threshs, values, ':');
+hold on;
+% Plot average across runs
+plot(threshs, mean(values, 1), 'LineWidth', 2);
+
+title(tpm_type);
+ylabel('\Phi');
+xlabel('thresh value');
+
+
+
+
