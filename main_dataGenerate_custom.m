387e340b990756ac2dfd0e77a04f24fadb5ab8cb
Angus Leung <prototype032@gmail.com>
2020-07-13 14:37:21 +1000

    added spiking (equal value for all spikes) and resetting to baseline after spike

--- main_dataGenerate_custom.m
+++ main_dataGenerate_custom.m
@@ -22,7 +22,8 @@ nSamples = 200000;
 nRuns = 10;
 
 %% Model parameters
-model_type = 'NLbidirNoInstOrder1Thresh1'; % name of the model
+model_type = 'NLbidirNoInstOrder1Thresh0-6'; % name of the model
+%model_type = 'NLbidirNoInstOrder1Thresh0-7SpikeReset';
 
 model_params = struct();
 
@@ -32,13 +33,13 @@ model_params.constant = [0; 0];
 % Linear connection matrix (nodes x nodes x time-lag)
 % Only linear effects from self, none from others
 model_params.A =[...
-    -0.3 0;...
-    0 -0.3];
+    -0.1 0;...
+    0 -0.1];
 
 % Nonlinear connection matrix (nodes x nodes x time-lag)
 model_params.B = [...
-    0 0.7;...
-    0.7, 0];
+    0 0.9;...
+    0.9, 0];
 
 % Instantaneous noise covariance (nodes x nodes)
 % To generate noise from covariance:
@@ -49,10 +50,28 @@ model_params.Covariance = [...
     0 0.5];
 
 % Nonlinear function name ('NL' = 'nonlinear')
-model_params.NL_func = 'threshSpike';
+model_params.NL_func = 'thresh';
 model_params.NL_params = struct();
-model_params.NL_params.thresh = [0.5; 0.5]; % (nodes x 1)
-model_params.NL_params.spike = [1; 1]; % (nodes x 1)
+model_params.NL_params.thresh = [0.6; 0.6]; % (nodes x 1)
+
+% If value exceeds thresohld, spike to given value
+model_params.NL_params.spike = 0;
+model_params.NL_params.spikeVals = model_params.NL_params.thresh; % (nodes x 1)
+
+% Reset if thresholds exceeded
+model_params.NL_params.reset = 0;
+model_params.NL_params.resetVals = -model_params.NL_params.thresh;
+
+% Check for stability (so system doesn't explode)
+%   Not sure if check is correct (and what about imaginary eigenvalues?)
+%   Check applies to linear systems
+% Note for stability/stationarity:
+%   https://kevinkotze.github.io/ts-7-var/#fn6 - see section 1.3
+%   Lutkepohl, H. 2005. New Introduction to Multiple Time Series Analysis. Heidelberg: Springer-Verlang.
+eigs = eig(det(model_params.A + model_params.B));
+if any(eigs > 1)
+    warning('VAR probably unstable');
+end
 
 %% Simulate data
 % See MATLAB functions simulate(varm) and filter(varm) for reference
@@ -81,10 +100,32 @@ for run = 1 : nRuns
     % Step through time to determine samples
     for t = lags + 1 : nSamples + lags
         for lag = 1 : size(model_params.A, 3)
+            
             % Add lagged components to the noise
             Y(:, t) = Y(:, t) +...
                 model_params.A(:, :, lag) * Y(:, t-lag) +...
                 model_params.B(:, :, lag) * feval(model_params.NL_func, Y(:, t-lag), model_params.NL_params);
+            
+            % Spike
+            if model_params.NL_params.spike == 1
+                for node = 1 : size(model_params.A, 1)
+                    if Y(node, t) >= model_params.NL_params.thresh(node)
+                        Y(node, t) = model_params.NL_params.spikeVals(node);
+                    end
+                end
+            end
+            
+            % Reset if previous value met threshold
+            % Resets will override all of the previous things
+            %   TODO - move this part to the top?
+            if model_params.NL_params.reset == 1
+                for node = 1 : size(model_params.A, 1)
+                    if Y(node, t-1) >= model_params.NL_params.thresh(node)
+                        Y(node, t) = model_params.NL_params.resetVals(node);
+                    end
+                end
+            end
+            
         end
     end
     
