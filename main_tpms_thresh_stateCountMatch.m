569c2f2acc361564d9231f32614a708210881f52
Angus Leung <prototype032@gmail.com>
2020-07-13 14:43:09 +1000

    added functionality to binarise based on threshold values, instead of percentiles

--- main_tpms_thresh_stateCountMatch.m
+++ main_tpms_thresh_stateCountMatch.m
@@ -10,9 +10,10 @@ Builds TPMs
 samplesPerState = 200;
 
 % Binarise method
-binariseMethod = 'percSplit';
+binariseMethod = 'threshSplit';
 
 % Binarise parameters
+threshs = (10:2:90);
 threshs = (10:2:90);
 binariseParams = struct();
 
@@ -25,7 +26,7 @@ downMethod = 'binAverage';
 
 %% Setup
 
-prefix = 'NLbidirNoInstOrder1Thresh0_nSamples200000_nRuns10';
+prefix = 'NLbidirNoInstOrder1Thresh0-6SpikeReset_nSamples200000_nRuns10';
 
 % Source data
 source_dir = 'sim_data/';
@@ -40,9 +41,29 @@ tpm_string = [...
 mkdir('tpms', [prefix tpm_string]);
 out_dir = ['tpms/' prefix tpm_string '/'];
 
+% Convert percentile thresholds to actual values
+% Overwrites original threshs vectors
+if strcmp(binariseMethod, 'threshSplit')
+    thresh_min = prctile(data(:), min(threshs));
+    thresh_max = prctile(data(:), max(threshs));
+    
+    % Round to nearest 0.05
+    % Ceiling for min
+    thresh_min = 5*ceil((thresh_min*100)/5)/100;
+    % Floor for max
+    thresh_max = 5*floor((thresh_max*100)/5)/100;
+    
+    % Create new thresholds
+    threshs = (thresh_min:0.05:thresh_max);
+    thresh_values = repmat(threshs', [1 size(data, 2), size(data, 4), length(taus)]);
+else
+    % Thresh values determined when building TPMs
+    thresh_values = zeros(length(threshs), size(data, 2), size(data, 4), length(taus));
+end
+
 %% Build TPMs with constant number of samples per source state
 
-thresh_values = zeros(length(threshs), size(data, 2), size(data, 4), length(taus));
+broken = 0;
 for thresh_c = 1 : length(threshs)
     thresh = threshs(thresh_c);
     disp(thresh);
@@ -78,12 +99,19 @@ for thresh_c = 1 : length(threshs)
             [data_binarised, thresh_values(thresh_c, :, run ,tau)] = binarise_perc(data_resampled, binariseParams);
             nValues = 2;
         elseif strcmp(binariseMethod, 'threshSplit')
-            data_binarised = binarise_threshValue(data_resampled, thresh);
+            data_binarised = binarise_threshValue(data_resampled, binariseParams);
             nValues = 2;
         end
         
         % Build TPM
-        [tpm, state_counters] = build_tpm_equalStates(data_binarised, step, nValues, samplesPerState);
+        try
+            [tpm, state_counters] = build_tpm_equalStates(data_binarised, step, nValues, samplesPerState);
+        catch
+            thresh_c = thresh_c - 1;
+            threshs = threshs(1:thresh_c);
+            broken = 1;
+            break
+        end
         
         % Save TPM
         out_suffix = [...
@@ -92,6 +120,10 @@ for thresh_c = 1 : length(threshs)
             '_run' num2str(run)];
         save([out_dir out_suffix], 'tpm', 'state_counters', 'nValues');
         
+    end
+    
+    if broken == 1
+        break;
     end
     
     toc
